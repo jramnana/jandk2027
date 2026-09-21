a50c5f1ccfd2d5bc8738374d55a91445c13c9060
jramnana <8vwfcb8k5k@privaterelay.appleid.com>
2026-09-20 18:11:25 -0700

    Update room-booking-config.js

--- js/room-booking-config.js
+++ js/room-booking-config.js
@@ -1,3 +1,3 @@
 // Paste the NEW room-booking Apps Script web app /exec URL here after deployment.
 // This is separate from the existing RSVP endpoint. Leave blank to show "Rooms are coming soon".
-window.ROOM_BOOKING_CONFIG = Object.freeze({ appUrl: '' });
+window.ROOM_BOOKING_CONFIG = Object.freeze({ appUrl: 'https://script.google.com/macros/s/AKfycbyEF_Qz3nz8Wwk7zq3mHtByLEEWuPqlLyL84hRkqDsQNvO9EnvmgKUFI04ul7_T2Yyq/exec' });
